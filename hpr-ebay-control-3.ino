f2157b796e4653e3e59b1a7df2edeb73b4d221e7
Mitch Miller <socmodder@gmail.com>
2023-08-30 22:08:14 -0600

    optimization

--- hpr-ebay-control-3.ino
+++ hpr-ebay-control-3.ino
@@ -11,13 +11,14 @@ HPR::MitchLED ledUtils;
 HPR::MitchRelay relayUtils;
 
 float starting_alt = 0;
-float current_alt = 0;
+float current_alt = -10;
 float max_alt = 0;
 bool launch_initiated = false;
 bool apogee = false;
+bool chute_deployed = false;
 uint16_t apogee_counter = 0;
 
-bool debug = true;
+bool debug = false;
 uint16_t counter = 0;
 uint16_t counter_max = 10;
 
@@ -30,7 +31,8 @@ void setup() {
 
   if (!mpl.begin()) {
     Serial.println("Could not find MPL sensor. Check wiring.");
-    while(1);
+    while (1)
+      ;
   }
   mpl.setMode(MPL3115A2_ALTIMETER);
   Serial.println("MPL3115A2 OK!");
@@ -40,15 +42,68 @@ void setup() {
   ledUtils.initLED();
   relayUtils.initRelay();
 
-  starting_alt = mpl.getLastConversionResults(MPL3115A2_ALTITUDE);
+  getAltReading();
 
-  ledUtils.blinkFast(3, GREEN);
+  starting_alt = mpl.getLastConversionResults(MPL3115A2_ALTITUDE);
+  current_alt = starting_alt;
+
+  ledUtils.blinkFast(4, GREEN);
 }
 
 /***********************************************
  **************** LOOP *************************
  ***********************************************/
 void loop() {
+  if (debug) {
+    if (counter == counter_max) {
+      ledUtils.blinkFast(10, MAGENTA);
+
+      triggerDeployment();
+
+      while (1) {
+        delay(5000);
+      }
+    }
+    counter++;
+  }
+
+  getAltReading();
+  current_alt = mpl.getLastConversionResults(MPL3115A2_ALTITUDE);
+  // if current alt is more than 2 meters above starting, then rocket has launched
+  if (current_alt > starting_alt + 2) {
+    launch_initiated = true;
+  }
+
+  if (launch_initiated) {
+    if (apogee) {
+      // Turn on relay 2
+      triggerDeployment();
+      chute_deployed = true;
+    }
+
+    if (current_alt > max_alt) {
+      max_alt = current_alt;
+      apogee_counter = 0;
+    } else if (current_alt + 10 < max_alt) {
+      apogee_counter++;
+      apogee = apogee_counter >= 3;
+    }
+
+    String ts = sdUtils.getTimeStamp();
+    sdUtils.openFile();
+    sdUtils.writeDataLineBlocking(ts + " " + String(current_alt));
+    sdUtils.closeFile();
+
+    ledUtils.blinkFast(1, BLUE);
+
+    // now get results
+    Serial.println(current_alt);
+
+    delay(200);
+  }
+}
+
+void getAltReading() {
   // start a conversion
   Serial.println("Starting a conversion.");
   mpl.startOneShot();
@@ -59,47 +114,8 @@ void loop() {
   while (!mpl.conversionComplete()) {
     count++;
   }
-  Serial.print("Done! Counted to "); Serial.println(count);
-  
-  String ts = sdUtils.getTimeStamp();
-  current_alt = mpl.getLastConversionResults(MPL3115A2_ALTITUDE);
-
-  if (current_alt > max_alt) {
-    max_alt = current_alt;
-    apogee_counter = 0;
-  } else if (current_alt + 30 < max_alt) {
-    apogee_counter++;
-    apogee = apogee_counter >= 3;
-  }
-
-  if (apogee) {
-    // Turn on relay 2 
-    triggerDeployment();
-  }
-
-  sdUtils.openFile();
-  sdUtils.writeDataLineBlocking(ts + " " + String(current_alt));
-  sdUtils.closeFile();
-
-  ledUtils.blinkFast(1, BLUE);
-
-  // now get results
-  Serial.println(current_alt);
-
-  delay(1000);  
-
-  if (debug) {
-    if (counter == counter_max) {
-      ledUtils.blinkFast(10, MAGENTA);
-
-      triggerDeployment();
-
-      while(1) {
-        delay(5000);
-      }
-    }
-    counter++;
-  }
+  Serial.print("Done! Counted to ");
+  Serial.println(count);
 }
 
 void triggerDeployment() {
